d85a3c7fd2c294b74cb37958a4c85531980a7af3
Minsang Choi <radiofun8@gmail.com>
2026-01-23 21:30:30 -0800

    Initial Commit

---
+++ LightingSim/LightingSimApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightingSimApp.swift
+//  LightingSim
+//
+//  Created by Minsang Choi on 1/23/26.
+//
+
+import SwiftUI
+
+@main
+struct LightingSimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
